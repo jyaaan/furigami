48eb2a31524607f810f3f65732ce60a93dfe2e5c
jyaaan <john.yamashiro@gmail.com>
2024-10-08 15:41:16 -0700

    updating kuroshiro type def

--- src/kuroshiro.d.ts
+++ src/kuroshiro.d.ts
@@ -1,9 +1,8 @@
 declare module 'kuroshiro' {
-    // Add type definitions here as you use them in your project
-    class Kuroshiro {
-        init(analyzer?: any): Promise<void>;
-        convert(text: string, options?: any): Promise<string>;
-    }
+  class Kuroshiro {
+    init(analyzer?: any): Promise<void>;
+    convert(text: string, options?: any): Promise<string>;
+  }
 
-    export = Kuroshiro;
+  export = Kuroshiro;
 }
